6aa2970f7e94f8ef802b3c6cf559bb6ae974af12
Stefano Gioia <stefanogch@gmail.com>
2015-06-12 21:26:40 -0400

    cambie la visibilidad de movementEnabled a protected

--- src/com/vidaric/main/MovableGameObject.java
+++ src/com/vidaric/main/MovableGameObject.java
@@ -6,7 +6,7 @@ public abstract class MovableGameObject extends GameObject implements Movable {
 	public MovableGameObject(Vector3f newPosition) {
 		super(newPosition);
 	}
-	private boolean movementEnabled = false;
+	protected boolean movementEnabled = false;
 	public boolean canMove(){return movementEnabled;}
 	public void enableMovement(){movementEnabled=true;}
 	public void disableMovement(){movementEnabled=false;}
